caaa0af92eeaf103f43169d168bf8086a849f5f9
ebedinin <bedinin@gmail.com>
2021-11-14 09:17:38 +1000

    7

--- learn.js
+++ learn.js
@@ -1,9 +1,19 @@
-function checkScope() {
-    let i = 'function scope';
-    if (true) {
-        let i = 'block scope';
-        console.log('Block scope i is: ', i);
-    }
-    console.log('Function scope i is: ', i);
-    return i;
+const s = [5, 7, 2];
+
+function editInPlace() {
+    const a = s[2], b = s[1], c = s[0];
+    s[0] = a;
+    s[1] = c;
+    s[2] = b;
 }
+
+console.log(s);
+editInPlace();
+console.log(s);
+
+
+/*
+
+s.sort((a, b) => {
+    return a > b
+});*/
