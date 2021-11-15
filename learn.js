1094f0d526e9f40de93a88d3eae473c4b48a0ff7
ebedinin <bedinin@gmail.com>
2021-11-15 23:02:21 +1000

    45

--- learn.js
+++ learn.js
@@ -1,3 +1,10 @@
-let a = 8, b = 6;
-[a,b] = [b,a]
-// Only change code below this line
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function removeFirstTwo(list) {
+    // Only change code below this line
+    const [, , ...arr] = list; // Change this line
+    // Only change code above this line
+    return arr;
+}
+
+const arr = removeFirstTwo(source);
