f6bcb7b43e4713dc93815ccb0dad6992696d6936
ebedinin <bedinin@gmail.com>
2021-11-16 23:00:40 +1000

    97

--- learn.js
+++ learn.js
@@ -1,10 +1,13 @@
 const makeServerRequest = new Promise((resolve, reject) => {
-    // responseFromServer represents a response from a server
-    let responseFromServer;
+    // responseFromServer is set to true to represent a successful response from a server
+    let responseFromServer = true;
 
-    if(responseFromServer) {
+    if (responseFromServer) {
         resolve("We got the data");
     } else {
         reject("Data not received");
     }
+});
+makeServerRequest.then(result => {
+    console.log(result)
 });
