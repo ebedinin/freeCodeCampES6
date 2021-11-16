6a59eacaf1f57a1ba572d6879873fa4b662885e3
ebedinin <bedinin@gmail.com>
2021-11-16 21:28:21 +1000

    52

--- learn.js
+++ learn.js
@@ -1,12 +1,13 @@
-const stats = {
-    max: 56.78,
-    standard_deviation: 4.34,
-    median: 34.54,
-    mode: 23.87,
-    min: -0.75,
-    average: 35.85
+const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
 };
+function makeList(arr) {
+    // Only change code below this line
+    const failureItems = arr.map((value)=>`<li class="text-warning">${value}</li>`,)
 
-// Only change code below this line
-const half = ({max,min}) => (max + min) / 2.0;
-// Only change code above this line
+    return failureItems;
+}
+
+const failuresList = makeList(result.failure);
