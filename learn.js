9b47f6cb9841056b1a4f7901700a2cbd1b11b1fa
ebedinin <bedinin@gmail.com>
2021-11-16 22:08:30 +1000

    72

--- learn.js
+++ learn.js
@@ -1,18 +1,7 @@
-// Only change code below this line
-
-// Only change code above this line
-class Thermostat{
-    constructor(temp){
-        this._temp=temp
-    }
-    set temperature (t){
-        this._temp = t*9.0/5+32;
-    }
-    get temperature(){
-        return 5/9*(this._temp-32);
-    }
+export const uppercaseString = (string) => {
+    return string.toUpperCase();
 }
-const thermos = new Thermostat(76); // Setting in Fahrenheit scale
-let temp = thermos.temperature; // 24.44 in Celsius
-thermos.temperature = 26;
-temp = thermos.temperature; // 26 in Celsius
+
+export const lowercaseString = (string) => {
+    return string.toLowerCase()
+}
