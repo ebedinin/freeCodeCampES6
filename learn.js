b631ac8297223ee9b36be32a91d6471e61fcbd46
ebedinin <bedinin@gmail.com>
2021-11-16 21:58:01 +1000

    66

--- learn.js
+++ learn.js
@@ -1,10 +1,18 @@
 // Only change code below this line
 
 // Only change code above this line
-class Vegetable{
-    constructor(name){
-        this.name = name;
+class Thermostat{
+    constructor(temp){
+        this._temp=temp
+    }
+    set temperature (t){
+        this._temp = t*9.0/5+32;
+    }
+    get temperature(){
+        return 5/9*(this._temp-32);
     }
 }
-const carrot = new Vegetable('carrot');
-console.log(carrot.name); // Should display 'carrot'
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
